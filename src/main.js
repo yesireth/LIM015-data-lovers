bbc4177b62b1c70e5bf8d5545b244c3668da07e6
yesireth <yesireth88@gmail.com>
2021-06-22 19:34:04 -0500

    se agregaron eventos del DOM

--- src/main.js
+++ src/main.js
@@ -1,6 +1,51 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import { filterData } from './data.js';
 
-console.log(example, data);
+//import data from './data/athletes/athletes.js';
+
+//let btnSport= document.getElementById("sport");
+// let btnAthletes= document.getElementById("athletes");
+//let btnCountries= document.getElementById("countries");
+
+
+let navigation = document.getElementsByClassName("button");
+
+for (let i = 0; i < navigation.length; i++) {
+
+  navigation[i].addEventListener("click", () => {
+
+    let category = navigation[i].value;
+    console.log(category);
+
+    filterData(category);
+    showAthletes(arraylist);
+  })
+}
+function showAthletes(arraylist){
+console.log(arraylist);
+  // const athletes= document.querySelector('.athletes');
+  // const resultathletes= document.createElement('div');
+  // resultathletes.innerHTML= ´ 
+  
+
+  
+  // ´
+  // athletes.appendChild(resultathletes);
+
+
+}
+
+// const athletes= document.querySelector('.athletes');
+//  const resultathletes= document.createElement('div');
+//  resultathletes.innerHTML= ´ 
+ 
+ 
+ 
+//  ´
+
+//  athletes.appendChild(resultathletes);
+// console.log(resultathletes);
+
+
+// btnSport.addEventListener("click", filterData);
+// btnAthletes.addEventListener("click", filterData);
+// btnCountries.addEventListener("click", filterData);
