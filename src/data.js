c722d879e71e70ec0cc74e88bf18287648dd8574
yesireth <yesireth88@gmail.com>
2021-06-24 22:10:40 -0500

    cambios con datos filtrados de atletas y deportes

--- src/data.js
+++ src/data.js
@@ -1,40 +1,40 @@
 import athletes from "./data/athletes/athletes.js";
-console.log(athletes.athletes)
+//console.log(athletes.athletes)
 
-// export const filterData = (category) => {
-//   let filteredDate;
-//   let arrayList = [];
+export const filterData = (category) => {
+  let filteredDate;
+  let arrayList = [];
   
-//   for (let index = 0; index < athletes.athletes.length; index++) {
+  for (let index = 0; index < athletes.athletes.length; index++) {
 
-//     if (category === 'Atletas') {
-//       filteredDate = athletes.athletes[index];
-//       console.log(filteredDate )
-//       arrayList.push(filteredDate);
-//     } else if (category === 'Paises') {
-//       filteredDate = athletes.athletes[index].team;
-//       arrayList.push(filteredDate);
-//     } else if (category === 'Deportes') {
-//       filteredDate = athletes.athletes[index].sport;
-//       arrayList.push(filteredDate);
-//     }
+    if (category === 'Atletas') {
+      filteredDate = athletes.athletes[index];
+      //console.log(filteredDate )
+      arrayList.push(filteredDate);
+    } else if (category === 'Paises') {
+      filteredDate = athletes.athletes[index].team;
+      arrayList.push(filteredDate);
+    } else if (category === 'Deportes') {
+      filteredDate = athletes.athletes[index].sport;
+      arrayList.push(filteredDate);
+    }
 
-//     const athlete = { 
-//       name: athletes.athletes[index].name,
-//       country:athletes.athletes[index].team,
-//       gender:athletes.athletes[index].gender,
-//       sport:athletes.athletes[index].sport,
-//       medal:athletes.athletes[index].medal,
-//       age:athletes.athletes[index].age,
-//       height:athletes.athletes[index].height,
-//       weight: athletes.athletes[index].weight,
-//     } 
+    const athlete = { 
+      name: athletes.athletes[index].name,
+      country:athletes.athletes[index].team,
+      gender:athletes.athletes[index].gender,
+      sport:athletes.athletes[index].sport,
+      medal:athletes.athletes[index].medal,
+      age:athletes.athletes[index].age,
+      height:athletes.athletes[index].height,
+      weight: athletes.athletes[index].weight,
+    } 
 
-//     console.log(athlete)
-//   }
+    console.log(athlete)
+  }
 
-// return arrayList;
+return arrayList;
 
-// }
+}
 
   //console.log(Objathletas)}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // Importamos funciones desde data.js
 import { filterData } from './data.js';
-<<<<<<< HEAD
+
 // Elementos del HTML
 const navigation = document.getElementsByClassName("button");
 const containerAthletes = document.querySelector('.containerAthletes');
@@ -8,85 +8,82 @@ const containerWomen = document.querySelector('.women');
 const containerFeatured = document.querySelector('.featured');
 const containerIntro = document.querySelector('.intro');
 const containerSlide = document.querySelector('.containerSlide');
-
+const containerSport = document.querySelector('.containerSport');
+  
+// Elementos del HTML 
+//const navigation = document.getElementsByClassName("button");
+//const containerAthletes = document.querySelector('.athletes');
 // Variables globales
 let category = " ";
 let arrayData = " ";
 
 //Evento para los botones
-=======
+
 //import data from './data/athletes/athletes.js';
 
-// Elementos del HTML 
-const navigation = document.getElementsByClassName("button");
-const containerAthletes = document.querySelector('.athletes');
-
-// Variables Globales 
-let category;
 
 // Eventos a los botones 
->>>>>>> d29e1c13103f83ca0b498a8d7ccf731ca45ef332
+
 for (let i = 0; i < navigation.length; i++) {
   navigation[i].addEventListener("click", () => {
-<<<<<<< HEAD
+
 
     category = navigation[i].value;
     arrayData = filterData(category);
   
     showAthletes(arrayData);
     showSport(arrayData);
+    // Oculta informacion previa
+containerWomen.style.display = 'none';
+containerFeatured.style.display = 'none';
+containerIntro.style.display = 'none';
+containerSlide.style.display = 'none';
+
   })
+
 }
 
 function showAthletes(arrayData) {
-  // Oculta informacion previa
-  containerWomen.style.display = 'none';
-  containerFeatured.style.display =  'none';
-  containerIntro.style.display = 'none';
-  containerSlide.style.display = 'none';
 
   // Recorre el array y agrega un div por cada atleta
-  arrayData.forEach(element =>{ 
-    console.log(element.name);
-   const divAthlete= document.createElement('div');
-   divAthlete.classList.add("athlete")
-  divAthlete.innerHTML= `
+  arrayData.forEach(element => {
+   // console.log(element.name);
+    const divAthlete = document.createElement('div');
+    divAthlete.classList.add("athlete")
+    divAthlete.innerHTML = `
   <p>Nombre: ${element.name} </p>
   <p>Deporte: ${element.sport} </p>
-  //  `
-  containerAthletes.appendChild(divAthlete);
-  } );
-=======
-    category = navigation[i].value;
-    showAthletes();
+   `
+    containerAthletes.appendChild(divAthlete);
+
+  });
+}
+function showSport(arrayData) {
+  //containerAthletes.style.display = 'none';
+
+  console.log(arrayData);
+  arrayData.forEach(element => {
+    const divSport = document.createElement('div');
+    divSport.classList.add("sport");
+    divSport.innerHTML = `
+
+<p>Deporte: ${element}</p>
+
+`
+    containerSport.appendChild(divSport);
+    //console.log(element);
   })
 }
->>>>>>> d29e1c13103f83ca0b498a8d7ccf731ca45ef332
 
-// Funcion para mostrar en pantalla los resultados
-function showAthletes(){
-  console.log
-  const athletes = document.createElement('div');
-  athletes.innerHTML = `${filterData(category)}`
-  containerAthletes.appendChild(athletes)
-}
-function showSport(arrayData){ 
-arrayData.forEach(element => {
-console.log(element.sport)
-
-});
-}
-console.log(showSport);
 
 // }
-    
+
 // // const athletes= document.querySelector('.athletes');
 // //  const resultathletes= document.createElement('div');
  
 
 // //  athletes.appendChild(resultathletes);
 // // console.log(resultathletes);
-
 
 // // btnSport.addEventListener("click", filterData);
 // // btnAthletes.addEventListener("click", filterData);
