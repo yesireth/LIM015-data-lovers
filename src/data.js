65361d952eb73c299f1054688e79aa0b7e83053e
margaZM <aurazambrano227@gmail.com>
2021-07-06 21:03:09 -0500

    Agrega objeto statisticsData y ordena filterData Agrega metodos sumMedalsCountries y sumMedalsOfAthlete para la tabla de las estaditicas Ordena el objeto filterData con los metodos para remover datos duplicados de atletas, deportes y paises

--- src/data.js
+++ src/data.js
@@ -1,23 +1,43 @@
 import athletes from "./data/athletes/athletes.js";
 
-export const filterData =
-{
+export const filterData = {
+  removeDuplicateNames: (athletes) => {
+    let objAthletes = {};
+    const filterDuplicateAthletes = athletes
+    .filter(athlete => objAthletes[athlete.name] ? false : objAthletes[athlete.name] = true);
+    return filterDuplicateAthletes;
+  },
+  
+  selectedAthleteInformation: (selectedAthleteName, athletes)  => {
+    return athletes.filter(athlete => athlete.name == selectedAthleteName);
+  },
+  removeDuplicateDataArray: (category, athletes)  => {
+    const allData = athletes.map(athlete => athlete[category]);
+    // let uniqueSport = [...new Set(allSport)];
+    const uniqueData = allData.filter((item, index) => {
+      return allData.indexOf(item) === index;
+    })
+    return uniqueData;
+  },
+
   filterMultipleData: (category, minedad, maxedad) => {
     const gender = athletes.athletes.filter(a => a.gender == category);
     const medal = athletes.athletes.filter(a => a.medal == category);
     const age = athletes.athletes.filter(a => a.age >= minedad && a.age <= maxedad);
 
     if (category === "F" || category === "M") {
-
       return gender;
     } else if (category === "Gold" || category === "Silver" || category === "Bronze") {
       return medal;
     } else {
       return age;
     }
-  },
+  }
+}
+
+export const orderData = {
   orderedSelect: (sortItem, athletes) => {
-    let orderedarray
+    let orderedarray;
     if (sortItem == "A-Z") {
       orderedarray = athletes.sort(function (a, b) {
         if (a.name.toUpperCase() > b.name.toUpperCase()) {
@@ -44,51 +64,64 @@ export const filterData =
     }
     else if (sortItem == "menos-edad") {
       orderedarray = athletes.sort(function (a, b) {
-        if (a.age > b.age) {
-          return 1;
-        }
-        else if (a.age < b.age) {
-          return -1;
-        }
-        return 0;
+        // if (a.age > b.age) {
+        //   return 1;
+        // }
+        // else if (a.age < b.age) {
+        //   return -1;
+        // }
+        // return 0;
+        return a.age - b.age;
       }
       )
     }
     else if (sortItem == "mas-edad") {
       orderedarray = athletes.sort(function (a, b) {
-        if (a.age < b.age) {
-          return 1;
-        }
-        else if (a.age > b.age) {
-          return -1;
-        }
-        return 0;
+        // if (a.age < b.age) {
+        //   return 1;
+        // }
+        // else if (a.age > b.age) {
+        //   return -1;
+        // }
+        // return 0;
+        return b.age - a.age;
       }
       )
-
     }
-return orderedarray; 
+    return orderedarray; 
   }
 }
 
 export const statisticsData = {
-  sumGold: () => {
-    athletes.athletes.reduce((total, athlete) => {
-      total + athlete.medal;
-      console.log(total, athlete.medal)
-    },0)
+  sumMedalsCountries: (medal, athletes) => {
+    const medalsByCountry = athletes
+    .filter(athlete => athlete.medal === medal)
+    // .reduce((count, athlete) => (count[athlete.team] ? count[athlete.team] += 1 : count[athlete.team] = 1, count), [])
+    .reduce((count, athlete) => {
+      if (count[athlete.noc]){
+        count[athlete.noc] += 1
+      } else {
+        count[athlete.noc] = 1
+      }
+      return count;
+    }, [])
+    return medalsByCountry;
+  },
+  sumMedalsOfAthlethe: (nameAthlete, athletes) => {
+    const medalsByAthlete = athletes
+    .filter(athlete => athlete.name === nameAthlete)
+    .reduce((count, athlete) => (count[athlete.medal] ? count[athlete.medal] += 1 : count[athlete.medal] = 1, count), [])
+    return medalsByAthlete;
   }, 
-  sumSilver: () => {
-    athletes.athletes.reduce((total, athlete) => {
-      total + athlete.medal;
-      console.log(total, athlete.medal)
-    },0)
-  }, 
-  sumBronze: () => {
-    athletes.athletes.reduce((total, athlete) => {
-      total + athlete.medal;
-      console.log(total, athlete.medal)
-    },0)
-  }
+  // totalMedals: (athletes) => {
+  //   const totalMedals = athletes
+  //   .reduce((count, athlete) => (count[athlete.medal] ? count[athlete.medal] += 1 : count[athlete.medal] = 1, count), [])
+  //   return totalMedals;
+  // },
+  // sumMedalsByGender: (gender, athletes) => {
+  //   const medalsByGender= athletes
+  //   .filter(athlete => athlete.gender === gender)
+  //   .reduce((count, athlete) => (count[athlete.medal] ? count[athlete.medal] += 1 : count[athlete.medal] = 1, count), [])
+  //   return medalsByGender;
+  // }
 }
-
